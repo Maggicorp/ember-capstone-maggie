3284aea2f74c44089b92016415cb8bc6db9ec06d
Maggicorp <mvjcasey@gmail.com>
2017-06-09 14:15:58 -0400

    Adds back end url in config/enviroment.js
    Should connect to rail back end

--- config/environment.js
+++ config/environment.js
@@ -52,7 +52,7 @@ module.exports = function (environment) {
   if (environment === 'production') {
     ENV.rootURL = '/ember-capstone-maggie';
    ENV.locationType = 'hash';
-   ENV.apiHost = '<% replace with the URL to your deployed API %>';
+   ENV.apiHost = 'https://lit-citadel-67931.herokuapp.com/';
  }
 
   return ENV;
