0c4d6e44e0d30718c0689bb45a9ec03d6a7c0dbc
Maggicorp <mvjcasey@gmail.com>
2017-06-10 13:24:36 -0400

    Allows you to enter new information
    Starts with empty template, allows you to add to it

--- app/components/poem-create.js
+++ app/components/poem-create.js
@@ -2,20 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   newPoem: {
-    title: 'Yossa Buson Example Haiku',
-    firstline: 'A summer river being crossed',
-    secondline: 'how pleasing',
-    thirdline: 'with sandals in my hands'
+    title: null,
+    firstline: null,
+    secondline: null,
+    thirdline: null
   },
   actions: {
     submit() {
-      console.log('on submit')
+      console.log('after on submit')
       let data =
       this.get('newPoem')
-      // data.user = this.get('user')
       console.log('data is', data)
       this.sendAction('poemCreate', data);
-      this.set('newPoem', null);
+      this.set('newPoem.title', null);
+      this.set('newPoem.firstline', null);
+      this.set('newPoem.secondline', null);
+      this.set('newPoem.thirdline', null);
     }
   }
   })
