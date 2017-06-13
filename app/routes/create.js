c97c8682c4b251b44d1be4ff234c6598bac748e8
Maggicorp <mvjcasey@gmail.com>
2017-06-13 12:46:14 -0400

    Creates attribute and returns array with syllable sum
    need to reset syllabul sum and find a way to return and display it

--- app/routes/create.js
+++ app/routes/create.js
@@ -1,25 +1,29 @@
+// import store from 'store.js'
+
 import Ember from 'ember';
 
 export default Ember.Route.extend({
   ajax2: Ember.inject.service(),
-  totalSyl: Ember.computed( function() {
-    let count = 0
-    return count
-  }),
+  count: [],
   actions: {
     callapi (word) {
+      // let hat = 'hat'
+      // store.hat = hat
       console.log('did word make it', word)
       let url = 'https://wordsapiv1.p.mashape.com/words/' + word + '/syllables'
       console.log('call api in create.js');
-      console.log('computer is', this.get('totalSyl'))
       return this.get('ajax2').request(url, {
         method: 'GET',
         headers: {'X-Mashape-Key': 'KT2RseIfS9mshXfUPbWLcFk8QDorp1P2tRmjsnE9qkK1Wrs6es'}
       })
       .then((response) => {
-        // this.get('store').pushPayload(response)
-        // need to make a model for word count
         console.log(response)
+        let count = this.get('count')
+        console.log('count is', count)
+        count.push(response.syllables.count);
+        console.log('count is', count)
+        count.filter(n => true)
+        console.log('sum is', count.reduce((a, b) => a + b, 0));
         this.get('flashMessages')
         .success('The number of syllabuls in the word ' + word + ' is ' + response.syllables.count, {
         sticky: true,
